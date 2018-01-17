07c7c2d0bc7747e56ad47deb63aa0396d3df1a2e
leequer <leequer@qq.com>
2018-01-17 09:59:00 +0800

    添加layout注释

--- app/src/main/java/com/liqing/android_drag_move_demo/ActivityMain.java
+++ app/src/main/java/com/liqing/android_drag_move_demo/ActivityMain.java
@@ -137,7 +137,9 @@ public class ActivityMain extends Activity {
 
                         if (myInRangeOfView(mImage1, event)) {
                             Log.e(tag, "if");
-
+                            /**
+                             * 该方法是View的放置方法，在View类实现。调用该方法需要传入放置View的矩形空间左上角left、top值和右下角right、bottom值。这四个值是相对于父控件而言的。例如传入的是（10, 10, 100, 100），则该View在距离父控件的左上角位置(10, 10)处显示，显示的大小是宽高是90(参数r,b是相对左上角的)，这有点像绝对布局。
+                             */
                             //覆盖image1
                             mImage.layout((int) mImage1.getX(), (int) mImage1.getY(), (int) mImage1.getX() + mImage1.getWidth(), mImage1.getHeight());
 //                            //换位置
